e651fcf2639dab5267d802f5ee4f4c2ad0c28d2e
Ashokkumar <ashokkumarash96@gmail.com>
2023-06-05 10:15:03 +0530

    update

--- src/scenes/MySkills.js
+++ src/scenes/MySkills.js
@@ -4,9 +4,6 @@ import htmlSkill from "../assets/htmlicon.jpeg";
 import cssSkill from "../assets/cssicon.jpeg";
 import jsSkill from "../assets/jsicon.jpeg";
 import reactSkill from "../assets/reacticon.png";
-import mongoSkill from "../assets/mongoicon.png";
-import nodeSkill from "../assets/nodeeicon.png";
-import expressSkill from "../assets/expressicon.png";
 
 const MySkills = () => {
   return (
@@ -33,12 +30,10 @@ const MySkills = () => {
             Cupiditate, unde qui suscipit ad eum harum?
           </p>
         </motion.div>
-
         <div className='mt-16 md:mt-0'></div>
       </div>
-
       {/* SKILLS */}
-      <div className='md:flex md:justify-between mt-16'>
+      <div className='md:flex md:justify-between mt-16 place-content-center'>
         <motion.div
           className='md:w-1/3 mt-10'
           initial='hidden'
@@ -60,7 +55,6 @@ const MySkills = () => {
             </div>
           </div>
         </motion.div>
-
         <motion.div
           className='md:w-1/3 mt-10'
           initial='hidden'
@@ -82,7 +76,6 @@ const MySkills = () => {
             </div>
           </div>
         </motion.div>
-
         <motion.div
           className='md:w-1/3 mt-10'
           initial='hidden'
@@ -121,69 +114,6 @@ const MySkills = () => {
               <img
                 className='h-36 w-48 place-content-center'
                 src={reactSkill}
-                alt=''
-              />
-            </div>
-          </div>
-        </motion.div>
-        <motion.div
-          className='md:w-1/3 mt-10'
-          initial='hidden'
-          whileInView='visible'
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
-          }}
-        >
-          <div className='relative h-32'>
-            <div className='z-10 place-content-center'>
-              <img
-                className='h-36 w-48 place-content-center'
-                src={nodeSkill}
-                alt=''
-              />
-            </div>
-          </div>
-        </motion.div>
-        <motion.div
-          className='md:w-1/3 mt-10'
-          initial='hidden'
-          whileInView='visible'
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
-          }}
-        >
-          <div className='relative h-32'>
-            <div className='z-10 place-content-center'>
-              <img
-                className='h-36 w-48 place-content-center'
-                src={expressSkill}
-                alt=''
-              />
-            </div>
-          </div>
-        </motion.div>
-        <motion.div
-          className='md:w-1/3 mt-10'
-          initial='hidden'
-          whileInView='visible'
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
-          }}
-        >
-          <div className='relative h-32'>
-            <div className='z-10 place-content-center'>
-              <img
-                className='h-36 w-48 place-content-center'
-                src={mongoSkill}
                 alt=''
               />
             </div>
